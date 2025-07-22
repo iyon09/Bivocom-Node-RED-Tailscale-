d06d05e29fbfecc8cfa0a4d0080c8faa1b206f5f
iyon09 <164447849+iyon09@users.noreply.github.com>
2025-07-22 17:55:05 +0800

    Update DanLab_BV2.sh

--- DanLab_BV2.sh
+++ DanLab_BV2.sh
@@ -18,6 +18,17 @@ echo "__________________________________________________________________________
 
 
 sudo apt update
+sudo apt upgrade
+
+# Install Tailscale
+echo "Task : Inatalling Tailscale"
+sudo mkdir -p /etc/apt/sources.list.d/
+echo "deb [signed-by=/usr/share/keyrings/tailscale-archive-keyring.gpg] https://pkgs.tailscale.com/stable/ubuntu focal main" | sudo tee /etc/apt/sources.list.d/tailscale.list > /dev/null
+curl -fsSL https://pkgs.tailscale.com/stable/ubuntu/focal.noarmor.gpg | sudo tee /usr/share/keyrings/tailscale-archive-keyring.gpg > /dev/null
+
+# Update package lists again
+sudo apt update
+
 
 # Install Tailscale
 sudo apt install -y tailscale
